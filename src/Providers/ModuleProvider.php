aeaa29f09d1cdb4a5633f61d6a71c1d69b9034f6
Nasko <nasko@webfactory.bg>
2017-02-10 10:30:26 +0200

    Not linkable to a page (yet)

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -23,7 +23,7 @@ class ModuleProvider extends ServiceProvider
         );
 
         $modules = $this->app['config']['typicms']['modules'];
-        $this->app['config']->set('typicms.modules', array_merge(['currencies' => ['linkable_to_page']], $modules));
+        $this->app['config']->set('typicms.modules', array_merge(['currencies' => []], $modules));
 
         $this->loadViewsFrom(__DIR__.'/../resources/views/', 'currencies');
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'currencies');
